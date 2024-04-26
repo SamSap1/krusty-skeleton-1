5320942c3c1d3d2e6ea812723da9efc0ed8c5b5b
Sam Sapkins <se1075sa-s@student.lu.se>
2024-04-26 08:55:57 +0200

    Created connection, implemented def values, impl. getCustomers

--- krusty-skeleton/src/main/java/krusty/Database.java
+++ krusty-skeleton/src/main/java/krusty/Database.java
@@ -3,6 +3,10 @@ package krusty;
 import spark.Request;
 import spark.Response;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
@@ -15,19 +19,144 @@ public class Database {
 	 */
 
 	 //TO DO
-	private static final String jdbcString = "jdbc:mysql://localhost/krusty";
 
 	// For use with MySQL or PostgreSQL
 	private static final String jdbcUsername = "hbg07";
 	private static final String jdbcPassword = "sbz864po";
+	private static final String jdbcString = "jdbc:mysql://puccini.cs.lth.se/hbg07?user="+jdbcUsername+"&password=" +jdbcPassword;;
 
+
+	private static final String Customersdef = "INSERT INTO Customers(customerName, address) VALUES" + 
+	"   ('Finkakor AB', 'Helsingborg'), " +
+    "   ('Småbröd AB', 'Malmö')," +
+    "   ('Kaffebröd AB', 'Landskrona')," + 
+    "   ('Bjudkakor AB', 'Ystad')," +
+    "   ('Kalaskakor AB', 'Trelleborg')," + 
+    "   ('Partykakor AB', 'Kristianstad')," +
+    "   ('Gästkakor AB', 'Hässleholm')," +
+    "   ('Skånekakor AB', 'Perstorp')," + 
+ ";"
+;
+
+
+private static final String Cookiedef = "INSERT INTO Cookie (productName) VALUES" + 
+"   ('Almond delight')," +
+"   ('Amneris')," +
+"   ('Berliner')," + 
+"   ('Nut cookie')," +
+"   ('Nut ring')," +
+"    ('Tango')" +
+";"
+;
+
+private static final String Ingredientsdef = "INSERT INTO Ingredients (ingredientName, QuantityInStorage, Unit) VALUES" +
+    "   ('Bread crumbs', 500000, 'g'),"+
+    "   ('Butter', 500000, 'g'),"+
+    "   ('Chocolate', 500000, 'g'),"+
+	"   ('Chopped almonds', 500000, 'g'),"+
+    "   ('Cinnamon', 500000, 'g'),"+
+    "   ('Egg whites', 500000, 'ml'),"+
+	"   ('Eggs', 500000, 'g'),"+
+    "   ('Fine-ground nuts', 500000, 'g'),"+
+	"   ('Flour', 500000, 'g'),"+
+    "   ('Ground, roasted nuts', 500000, 'g'),"+
+    "   ('Icing sugar', 500000, 'g'),"+
+    "   ('Marzipan', 500000, 'g'),"+
+    "   ('Potato starch', 500000, 'g'),"+
+    "   ('Roasted, chopped nuts', 500000, 'g'),"+
+    "   ('Sodium bicarbonate', 500000, 'g'),"+
+    "   ('Sugar', 500000, 'g'),"+
+    "   ('Vanilla sugar', 500000, 'g'),"+
+    "   ('Vanilla', 500000, 'g'),"+
+    "   ('Wheat flour', 500000, 'g')"+
+	";"
+	;
+
+	private static final String Recipesdef = "INSERT INTO Recipe(productName, ingredientName, Quantity) VALUES" +
+	"VALUES( 'Almond delight', 'Butter', 400),"+
+	"VALUES( 'Almond delight', 'Chopped almonds', 279),"+
+	"VALUES( 'Almond delight', 'Cinnamon', 10),"+
+	"VALUES( 'Almond delight', 'Flour', 400),"+
+	"VALUES( 'Almond delight', 'Sugar', 270),"+
+	"VALUES( 'Amneris', 'Marzipan' 750),"+
+	"VALUES( 'Amneris', 'Butter' 250),"+
+	"VALUES( 'Amneris', 'Eggs' 250),"+
+	"VALUES( 'Amneris', 'Potato starch' 25),"+
+	"VALUES( 'Amneris', 'Wheat flour' 25),"+
+	"VALUES( 'Berliner', 'Butter', 250),"+
+	"VALUES( 'Berliner', 'Chocolate', 50),"+
+	"VALUES( 'Berliner', 'Eggs', 50),"+
+	"VALUES( 'Berliner', 'Flour', 350),"+
+	"VALUES( 'Berliner', 'Icing sugar', 100),"+
+	"VALUES( 'Berliner', 'Vanilla sugar', 5),"+
+	"VALUES( 'Nut cookie', 'Fine-ground nuts', 750),"+
+	"VALUES( 'Nut cookie', 'Ground, roasted nuts', 625),"+
+	"VALUES( 'Nut cookie', 'Bread crumbs', 125),"+
+	"VALUES( 'Nut cookie', 'Sugar', 375),"+
+	"VALUES( 'Nut cookie', 'Egg whites', 3.5),"+
+	"VALUES( 'Nut cookie', 'Chocolate', 50),"+
+	"VALUES( 'Nut Ring', 'Flour', 450),"+ 
+	"VALUES( 'Nut Ring', 'Butter', 450),"+
+	"VALUES( 'Nut Ring', 'Icing sugar', 190),"+
+	"VALUES( 'Nut Ring', 'Roasted, chopped nuts', 225),"+
+	"VALUES( 'Tango', 'Butter', 200),"+
+	"VALUES( 'Tango', 'Flour', 300),"+
+	"VALUES( 'Tango', 'Sodium bicarbonate', 4),"+
+	"VALUES( 'Tango', 'Sugar', 250),"+
+	"VALUES( 'Tango', 'Vanilla', 2)"+
+	";"
+	;   
+   
+
+
+	private Connection conn;
 	public void connect() {
-		// Connect to database here
+		try {
+			conn = DriverManager.getConnection(jdbcString, jdbcUsername, jdbcPassword);
+
+		} catch (SQLException e){
+			e.printStackTrace();
+			System.out.println("Connection failed");
+			System.exit(1);
+		}
+
+
+	}
+
+	public void close(){
+		try {
+			if (conn!= null){
+				conn.close();
+			}
+		} catch (SQLException e) {
+				e.printStackTrace();
+		}
+
+
 	}
 
 	// TODO: Implement and change output in all methods below!
 
 	public String getCustomers(Request req, Response res) {
+		try {
+			String query = "SELECT customerName AS name, address as address FROM Customer";
+			PreparedStatement conn = conn.prepareStatement(query);
+			ResultSet resultSet = prepare.executeQuery();
+			String json = Jsonizer.toJson(reusltset, "customers");
+			return json;
+
+		}
+
+		 catch (SQLxception e) {
+
+			//Kanske onödigt???
+					System.out.println("SQLMessage" + e.getMessage());
+					System.out.println("SQLState:" + e.getSQLState());
+					System.out.println("Error:" + e.getErrorCode());
+
+		}
+
+
 		return "{}";
 	}
 
